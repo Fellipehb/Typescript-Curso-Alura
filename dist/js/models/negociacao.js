5ebbe6db9e8d4e5f8d3c3411a2a864e3697fed25
Fellipehb <86789065+Fellipehb@users.noreply.github.com>
2023-01-10 11:18:07 -0300

    Retirando extensão Edge tools "erro strict"

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,23 +1,19 @@
-const { isConstructorDeclaration } = require("typescript");
-
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
-
-  isConstructorDeclaration(data, quantidade, valor) {
-    this._data       = data;
-    this._quantidade = quantidade;
-    this._valor      = valor;
-  }
-
-  get _data() {
-    return this._data;
-  }
-  get _quantidade() {
-    return this._quantidade;
-  }
-  get _valor() {
-    return this._valor;
-  }
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this.quantidade * this._valor;
+    }
 }
